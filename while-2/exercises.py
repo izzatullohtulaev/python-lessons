f2055a34edcb0a67f85c6126f8603efcda545e6d
Tulaev Muhammad <izzatullohman@gmail.com>
2023-06-28 21:54:01 +0500

    Fayllar yuklandi
    "Python dasturlash asoslari" darslarining "While Tsikli" darsining 2-qism fayllari yuklandi

---
+++ while-2/exercises.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 20:10:59 2023
+
+@author: Tulayev Izzat
+"""
+
+# 1
+# menu = {
+#     'lavash':26000,
+#     'hotdog':15000,
+#     'osh':30000,
+#     'kabob':50000,
+#     'somsa':8000,
+#     'qozon kabob':45000
+#     }
+# orders = {}
+# while True:
+#     buyurtma = input("Nima buyurtma qilasiz: ")
+#     dona = int(input("Nechta?: "))
+#     takrorlash = input("Yana nimadir buyurtma qilasizmi (ha/yo'q)?\n>>>")
+#     orders[buyurtma]=dona
+#     if takrorlash=='yo\'q':
+#         break
+# for order in orders:
+#     if order in menu:
+#         print(f"{orders[order]} ta {order}: {menu[order]*orders[order]}")
+#     else:
+#         print("Bizning restoranda", order, "yo'q")
+# print("Sizning buyurtmalaringiz: ")
+# for order in orders:
+#     print(order)
+
+# 2
+# mahsulotlar = {}
+# while True:
+#     mahsulot = input("Mahsulot: ")
+#     narx = int(input("Narx: "))
+#     mahsulotlar[mahsulot] = narx
+#     davom = input("Yana kiritasizmi: ")
+#     if davom == "yo'q":
+#         break
+# print(mahsulotlar)
+
+# 3
+# mahsulotlar = {"olma": 20000, "shaftoli": 25000, "tarvuz": 18000, "uzum": 22000}
+# buyurtmalar = []
+# while True:
+#     buyurtma = input("Nima buyurtma qilasiz: ")
+#     takrorlash = input("Yana nimadir buyurtma qilasizmi (ha/yo'q)?\n>>>")
+#     buyurtmalar.append(buyurtma)
+#     if takrorlash=='yo\'q':
+#         break
+# for buyurtma in buyurtmalar:
+#     if buyurtma in mahsulotlar:
+#         print(buyurtma, "bizda", mahsulotlar[buyurtma])
+#     else:
+#         print("Bizda", buyurtma, 'yo\'q')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ while-2/while_cycle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 15:55:08 2023
+
+@author: muhammad
+"""
+
+# print("Yaqin do'stlaringiz ro'yxatini tuzamiz")
+# ismlar = []
+# n = 1
+# while True:
+#     savol = f"{n}-do'stingizni ismini kiriting: "
+#     ism = input(savol)
+#     ismlar.append(ism)
+#     takrorlash = input("Yana malumot qo'shasizmi (ha/yo'q): ")
+#     n+=1
+#     if takrorlash=='yo\'q':
+#         break
+# print("Do'stlaringiz ro'yxati: ")
+# for ism in ismlar:
+#     print(f"\t{ism.title()}")
+
+# dostlar = {}
+# n = 1
+# while True:
+#     ism = input(f"{n}-do'stingizni ismini kiriting: ")
+#     yosh = int(input(f"{ism.title()}ning yoshini kiriting: "))
+#     n+=1
+#     dostlar[ism] = yosh
+#     takrorlash = input("Yana malumot kiritasizmi (ha/yo'q): ")
+#     if takrorlash=='yo\'q':
+#         break
+# print("Do'stlaringiz haqida siz kiritgan malumotlar: ")
+# for dost, yosh in dostlar.items():
+#     print(f"{dost.title()}ning yoshi {yosh} da")
+
+
+# cars = ['lacetti', 'nexia', 'toyota', 'nexia', 'audi', 'malibu', 'nexia']
+# while 'nexia' in cars:
+#     cars.remove('nexia')
+# print(cars)
+
+
+# cars = ['lacetti', 'nexia', 'toyota', 'nexia', 'audi', 'malibu', 'nexia', 'lacetti']
+# car = 'lacetti'
+# while car in cars:
+#     cars.remove(car)
+# print(cars)
+
+
+# talabalar = ['hasan', 'husan', 'olim', 'ali', 'vali']
+# baholangan = {}
+# while talabalar:
+#     talaba = talabalar.pop()
+#     baho = int(input(f"{talaba.title()}ning bahosini qo'ying: "))
+#     baholangan[talaba] = baho
+#     print(f"Talaba {talaba.title()} baholandi.")
+# print(baholangan)
+
+
+
+
+
+

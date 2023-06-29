65b812e66ff9a6c2455df4705d0d13519137f12b
Tulaev Muhammad <izzatullohman@gmail.com>
2023-06-29 07:29:52 +0500

    Fayllar yuklandi
    "Python dasturlash asoslari" darslarining "Funksiyalar"(functions) darslarining 1-qism fayllari yuklandi

---
+++ functions-1/exercises.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 07:10:48 2023
+
+@author: muhammad
+"""
+
+# 1
+# def print_byear(ism, yosh):
+#     """Bu funksiya foydalanuvchidan ismini 
+#     va yoshini qabul qilib olib,
+#     uning tug'ilgan yilini chiqaradi"""
+#     print(ism.title(), str(2023-yosh)+"-yilda tug'ilgan")
+# print_byear('izzat', 14)
+
+# 2
+# def print_square_and_cube(son):
+#     print("Kvadrat:", son**2)
+#     print("Kub:", son**3)
+# print_square_and_cube(2)
+
+# 3
+# def odd_or_even(son):
+#     if son%2==0:
+#         print("Juft son")
+#     else:
+#         print("Toq son")
+# odd_or_even(6)
+
+# 4
+# def bigger(a, b):
+#     if a>b:
+#         print(a)
+#     elif a<b:
+#         print(b)
+#     else:
+#         print("Teng")
+# bigger(4, 3)
+
+# 5
+# def x_y_root(x, y):
+#     print(x**y)
+# x_y_root(2, 3)
+
+# 6
+# def x_y_root(x, y=2):
+#     print(x**y)
+# x_y_root(2)
+
+# 7
+# def bolinish_alomatlari(son):
+#     numbers = list(range(2,11))
+#     for number in numbers:
+#         if son%number==0:
+#             print(son, number, "ga qoldiqsiz bo'linadi")
+#         else: continue
+# bolinish_alomatlari(70)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ functions-1/functions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 29 05:21:45 2023
+
+@author: Tulayev Izzat
+"""
+
+# def salom_ber():
+#     """Salom beruvchi funksiya"""
+#     print("Assalomu aleykum")
+# salom_ber()
+
+# def salom_ber(ism):
+#     """Foydalanuvchidan ismini qabul qilib, 
+#     unga salom beruvchi funksiya"""
+#     print(f"Assalomu aleykum, hurmatli {ism.title()}!")
+# salom_ber('hasan')
+# salom_ber('olim')
+# # salom_ber()
+
+# def toliq_ism(ism, familya):
+#     """Foydalanuvchidan ismi va familyasimi so'rab,
+#     ularni jamlab chiqaruvchi funksiya"""
+#     print("Foydalanuvchi ismi:", ism.title())
+#     print("Foydalanuvchi familyasi:", familya.title())
+# toliq_ism('izzat', 'tulayev')
+
+# def yosh_hisobla(ism, tugilgan_yil):
+#     """Foydalanuvchining yoshini hisoblab, 
+#     chiqaruvchi funksiya"""
+#     print(f"Foydalanuvchi {ism.title()} {2023-tugilgan_yil} yoshda")
+# yosh_hisobla('izzat', 2009)
+# yosh_hisobla(tugilgan_yil=2009, ism='izzat')
+
+# def yosh_hisobla(tugilgan_yil, joriy_yil=2023):
+#     """Foydalanuvchining yoshini hisoblab, 
+#     chiqaruvchi funksiya"""
+#     print(f"Siz {joriy_yil-tugilgan_yil} yoshdasiz")
+# yosh_hisobla(2009)
+# yosh_hisobla(2009, 2023) 
